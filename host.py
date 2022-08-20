5ba5a903ddcd1c9f3f75bf8f9a15f8653a7669ee
lo9ud <58418557+lo9ud@users.noreply.github.com>
2022-08-20 19:05:17 +0200

    did a lot

--- host.py
+++
@@ -1,8 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-    server_address = ('', 80)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
-print("Starting...")
-run()
